48d4ab8bfd5956d8e1dad2b3a13c685d828426d2
Lucas Sena <lucassena.rj@gmail.com>
2017-07-31 18:15:35 -0300

    Adicionado ids e funções
    Adicionado ID's e funções para os elementos.

--- app/src/main/java/com/lucassena/conversormoedas/MainActivity.java
+++ app/src/main/java/com/lucassena/conversormoedas/MainActivity.java
@@ -2,12 +2,40 @@ package com.lucassena.conversormoedas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mViewHolder.editValue = (EditText) findViewById(R.id.edit_value);
+        this.mViewHolder.textDollar = (TextView) findViewById(R.id.edit_value);
+        this.mViewHolder.textEuro = (TextView) findViewById(R.id.edit_value);
+        this.mViewHolder.buttonCalculate = (Button) findViewById(R.id.button_calculate);
+
+        this.mViewHolder.buttonCalculate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if(id == R.id.button_calculate){
+            this.mViewHolder.buttonCalculate.setText("Calculado");
+        }
+    }
+
+    private static class ViewHolder{
+        EditText editValue;
+        TextView textDollar;
+        TextView textEuro;
+        Button buttonCalculate;
     }
 }
